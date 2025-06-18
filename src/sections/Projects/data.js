0f45ec363c1db122fb1a1af96cc0f7740ad46fdd
soncoiz02 <soncoiz3107@gmail.com>
2025-06-18 21:43:22 +0700

    fix

--- src/sections/Projects/data.js
+++ src/sections/Projects/data.js
@@ -1,14 +1,22 @@
 import beauty from "@/assets/imgs/beauty paradise.jpg";
 import brighton from "@/assets/imgs/brighton.jpg";
 import portfolio from "@/assets/imgs/portfolio.png";
+import hangtho from "@/assets/imgs/hangthobg.png";
 
 export const projectData = [
   {
-    coverImg: beauty,
-    name: "Beauty Paradise",
-    desc: "Spa Booking Website",
-    link: "https://beautyparadise.vercel.app",
-    date: "02/2022 - 06/2022",
+    coverImg: hangtho,
+    name: "Hang Tho",
+    desc: "Coffee & Cocktail Bar Preview",
+    link: "https://hangtho.vercel.app",
+    date: "06/2025",
+  },
+  {
+    coverImg: portfolio,
+    name: "Tran Son Portfolio",
+    desc: "My Portfolio Website",
+    link: "https://remake-portfolio.vercel.app/",
+    date: " 12/2023",
   },
   {
     coverImg: brighton,
@@ -18,10 +26,10 @@ export const projectData = [
     date: "08/2022 - 10/2022",
   },
   {
-    coverImg: portfolio,
-    name: "Tran Son Portfolio",
-    desc: "My Portfolio Website",
-    link: "https://remake-portfolio.vercel.app/",
-    date: " 12/2023",
+    coverImg: beauty,
+    name: "Beauty Paradise",
+    desc: "Spa Booking Website",
+    link: "https://beautyparadise.vercel.app",
+    date: "02/2022 - 06/2022",
   },
 ];
